2f4f83e506ab1e6c5a6e76853553f335c7f530db
suresh298877 <121373235+suresh298877@users.noreply.github.com>
2023-11-08 17:41:10 +0530

    Update Login.js

--- src/components/Customer/Login.js
+++ src/components/Customer/Login.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 function Login(props) {
 
     const baseUrl = 'https://suresh2988.pythonanywhere.com/api/';
-    // const baseUrl = 'http://127.0.0.1:8000/api/';
+    {%// const baseUrl = 'http://127.0.0.1:8000/api/';%}
 
     const [formError, setFormError] = useState(false);
     const [errorMsg, setErrorMsg] = useState('');
